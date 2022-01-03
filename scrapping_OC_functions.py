6a919bb9b823f02e483e287ce85f6b6e7509d39a
Thomas SAINT-JEAN <thomassjn@gmail.com>
2022-01-03 23:46:51 +0300

    Ajout d'une fonctionnalité permettant le téléchargement des images produit des livres du site, classées dans des dossiers par catégorie de genre.

--- scrapping_OC_functions.py
+++ scrapping_OC_functions.py
@@ -37,8 +37,8 @@ def rec_category_books_urls(category_url):
 
         #récupération de la page produits suivante dans la catégorie
         if soup_page_category.find('li', class_="next"):
-            next = soup_page_category.find('li', class_="next").next_element
-            next_link = next['href']
+            next_line = soup_page_category.find('li', class_="next").next_element
+            next_link = next_line['href']
             url_category_splitted = category_url.split('/')
             url_category_splitted[-1] = next_link
             category_url = "/".join(url_category_splitted)
@@ -47,7 +47,7 @@ def rec_category_books_urls(category_url):
     return page_product_links
 
 
-def rec_book_details(book_url, description_titre=None):
+def rec_book_details(book_url):
     """Fonction de récupération des informations produits d'un livre.
         Cette fonction parcourt la page produit d'un livre et récupère les informations suivantes pour les stocker
         dans un dictionnaire:
@@ -84,30 +84,30 @@ def rec_book_details(book_url, description_titre=None):
     book_url_splitted = book_url.split("/")
     book_name = book_url_splitted[-2]
 
-    print(f"récupération des informations produit du livre {book_name} en cours")
+    print(f"Récupération des informations produit du livre {book_name} en cours")
     if response.status_code != 200:
-        print(f"Page {book_name} impossible a recuperer")
+        print(f"Page {book_name} impossible a récupérer")
     else:
-        print(f"Page {book_name} recuperee avec succes")
+        print(f"Page {book_name} récupérée avec succès")
 
     #transformation de la page en objet BeautifulSoup
-    page = response.content
-    soup = BeautifulSoup(page, "html.parser")
+    page_book = response.content
+    soup_page_book = BeautifulSoup(page_book, "html.parser")
 
     #recuperation du titre du livre
-    title = soup.find('div', class_="col-sm-6 product_main").find("h1")
+    title = soup_page_book.find('div', class_="col-sm-6 product_main").find("h1")
     dict_book['title'] = title.string
 
     #recuperation de la description du livre
-    if soup.find("div", id="product_description"):
-        description_titre = soup.find("div", id="product_description")
-        description_contenu = description_titre.next_sibling.next_sibling
-        dict_book['product_description'] = description_contenu.string
+    if soup_page_book.find("div", id="product_description"):
+        title_description = soup_page_book.find("div", id="product_description")
+        content_description = title_description.next_sibling.next_sibling
+        dict_book['product_description'] = content_description.string
     else:
         dict_book['product_description'] = "description de contenu absente"
 
     #recuperation des informations produits
-    product_informations = soup.find("table").find_all("td")
+    product_informations = soup_page_book.find("table").find_all("td")
     dict_book['universal_product_code'] = product_informations[0].string
     dict_book['category'] = product_informations[1].string
     dict_book['price_excluding_tax'] = product_informations[2].string
@@ -115,17 +115,17 @@ def rec_book_details(book_url, description_titre=None):
     dict_book['number_available'] = product_informations[5].string
 
     #recuperation de l'evaluation du livre
-    instock_availability = soup.find("p", class_="instock availability")
+    instock_availability = soup_page_book.find("p", class_="instock availability")
     review_rating = instock_availability.find_next("p")
     note = review_rating.attrs
     dict_book['review_rating'] = (note['class'][1])
 
     #recuperation de l'url de l'image du livre
-    infos_image = soup.find("img")
-    details_infos_image = infos_image.attrs
-    dict_book['image_url'] = (details_infos_image['src'])
-
+    infos_image = soup_page_book.find("img")
+    infos_image_details = infos_image.attrs
+    image_url_incomplete = infos_image_details['src']
+    image_url_incomplete_splitted = image_url_incomplete.split("/")
+    image_url = "https://books.toscrape.com/media/cache/" + image_url_incomplete_splitted[-3] + "/" + \
+                image_url_incomplete_splitted[-2] + "/" + image_url_incomplete_splitted[-1]
+    dict_book['image_url'] = image_url
     return dict_book
-
-
-
--- scrapping_OC_main.py
+++ scrapping_OC_main.py
@@ -1,17 +1,19 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
+import os
+import shutil
 
 from scrapping_OC_functions import rec_book_details, rec_category_books_urls
 
 #récupération des liens urls des catégories de livres et enregistrement de celles-ci dans une liste.
 url_books_to_scrape = "https://books.toscrape.com/index.html"
 response_web_site = requests.get(url_books_to_scrape)
-print("récupération de la page d'accueil du site en cours")
+print("Récupération de la page d'accueil du site en cours")
 if response_web_site.status_code != 200:
-    print("Page d'accueil impossible a recuperer")
+    print("Page d'accueil impossible a récupérer")
 else:
-    print("Page d'accueil recuperee avec succes")
+    print("Page d'accueil récupérée avec succès")
 home_page = response_web_site.content
 soup_home_page = BeautifulSoup(home_page, "html.parser")
 nav_list = soup_home_page.find('ul', class_="nav")
@@ -21,26 +23,41 @@ for link_category in links_categories:
     link_category = "https://books.toscrape.com/" + link_category["href"]
     categories_urls_list.append(link_category)
 categories_urls_list.pop(0)
-print(categories_urls_list)
-print("liste des catégories de livre constituée")
+print("Liste des catégories de livre constituée")
 
 
 #récupération des url des livres pour chaque catégorie du site
 category_books_dicts_list = []
 for category_url in categories_urls_list:
     category_url_splitted = category_url.split("/")
-    categoy_name = category_url_splitted[-2]
-    print(f"======================Récupération des livres de la catégorie {categoy_name} en cours=====================")
+    category_name = category_url_splitted[-2]
+    os.mkdir(f"Icones_{category_name}")
+    print(f"======================Récupération des livres de la catégorie {category_name} en cours====================")
     category_books_urls = rec_category_books_urls(category_url)
     #Constitution d'une liste de dictionnaires par catégorie. Un dictionnaire contient les données produit d'un livre
     for book_url in category_books_urls:
         book_dict = rec_book_details(book_url)
         category_books_dicts_list.append(book_dict)
+        book_url_splitted = book_url.split("/")
+        book_name = book_url_splitted[-2]
+        if len(book_name) > 100:
+            book_name = (book_name.split("-"))[0] + "_" + (book_name.split("-"))[1] + "_" + (book_name.split("-"))[2]
+        #récupération des icones produit des différents livres
+        image_url = book_dict['image_url']
+        r = requests.get(image_url, stream=True)
+        if r.status_code != 200:
+            print(f"Icone {book_name} impossible à récupérer")
+        else:
+            print(f"Icone {book_name} récupérée avec succès")
+        with open(f"Icone_{book_name}.jpg", 'wb') as f:
+            f.write(r.content)
+        source = f"Icone_{book_name}.jpg"
+        target = f"Icones_{category_name}"
+        shutil.move(source, target)
 
-#écriture des données produit des livres de chaque catégorie dans des fichiers .csv disctincts
-    with open(f"infos_recueil_{categoy_name}.csv", 'w', encoding="utf-8") as f:
+    #écriture des données produit des livres de chaque catégorie dans des fichiers .csv disctincts
+    with open(f"Infos_recueil_{category_name}.csv", 'w', encoding="utf-8") as f:
         w = csv.DictWriter(f, book_dict.keys())
         w.writeheader()
         for book_dict in category_books_dicts_list:
             w.writerow(book_dict)
-
